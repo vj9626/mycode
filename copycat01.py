236b433c80119e0132e6232254ece479215a29a0
vj <fake@fake.com>
2021-09-27 21:10:09 +0000

    end of monday

---
+++ copycat01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import shutil
+import os
+os.chdir("/home/student/mycode/")
+shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy")
+shutil.copytree("5g_research/", "5g_research_backup/")
+#!/usr/bin/env python3
+
+# import additional code to complete our task
+import shutil
+import os
+
+# move into the working directory
+os.chdir("/home/student/mycode/")
+
+# copy the fileA to fileB
+shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy")
+
+# copy the entire directoryA to directoryB
+shutil.copytree("5g_research/", "5g_research_backup/")
+
