383072ccfa1c5c79a4a5559469dcdab8d7ec2bf2
vj <fake@fake.com>
2021-10-01 17:59:55 +0000

    Learning about Classes

---
+++ smallscript.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+mynote = "print this string"
+print(mynote)
+#!/usr/bin/env python3
+
+"""
+Author: RZFeeser
+
+This program prints some things to the screen
+"""
+
+def main():
+    """Our main program"""
+    mynote = 'print this string' # define mynote
+    print(mynote)                # display our string
+
+main()
+
